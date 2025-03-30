909230da83b9be4b5b6d733f29f81b4a8ee1c559
DengChuan <133334175+debug-deng@users.noreply.github.com>
2025-03-30 11:09:09 +0800

    Update webagent.tsx

--- components/webagent.tsx
+++ components/webagent.tsx
@@ -7,7 +7,7 @@ interface WebAgentProps {
   title: string;
 }
 
-const WebAgent = ({ webAgentUrl,title }: WebAgentProps) => {
+const WebAgent = ({ webAgentUrl,title,url }: WebAgentProps) => {
   return (
     <div className="relative w-full h-full group dark:bg-darkBg flex flex-col justify-center items-center">
       {/* <div className="absolute -bottom-96 left-16 bg-cardPink w-[145%] h-full rounded-full dark:hidden" /> */}
@@ -26,7 +26,7 @@ const WebAgent = ({ webAgentUrl,title }: WebAgentProps) => {
       </div>
       <button className="absolute bg-white dark:bg-darkBg bottom-2 left-2 transition-all w-10 h-10 md:w-[2.75rem] md:h-[2.75rem] duration-500 ease-in-out group-hover:w-40 p-2 rounded-full hover:bg-default-100 border-2 border-transparent dark:border-knight">
         <div className="flex justify-center items-center">
-          <Link isExternal color="foreground" href="https://playcanv.as/b/6e3141a6">
+          <Link isExternal color="foreground" href={url}>
             <span className="text-sm md:text-medium text-nowrap hidden group-hover:block invisible group-hover:visible mr-1 animate-fade">
               {title}
             </span>
