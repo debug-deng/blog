615e7d724454c2e28545bc553716db7365072358
DengChuan <133334175+debug-deng@users.noreply.github.com>
2025-04-15 21:55:23 +0800

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -6,10 +6,11 @@ export const siteConfig = {
   links: {
     github: "https://github.com/debug-deng",
     linkedin: "https://www.innet.cloud",
-    email: "mailto:dengsuchuan@gmail.com",
+    email: "mailto:m17612011020@outlook.com",
   },
 };
 
 export const notionBlogConfig = {
-  blogParentId: "13eca92a-1fdc-807c-9c63-fd813f22449b",
+  //blogParentId: "13eca92a-1fdc-807c-9c63-fd813f22449b",
+  blogParentId:"1d69441e-2757-8046-9a7b-c16a7d4f2d27"
 };
